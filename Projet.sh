6bb25848d4555ee3a943ca54013db0ebfb4990be
Johnny <johnny.sass2001@gmail.com>
2025-04-24 16:11:27 +0200

    a

--- Projet.sh
+++ Projet.sh
@@ -1,13 +1,27 @@
-Name_dossier="main.c"
+#!/bin/bash
 
+# Nom du dossier contenant le proje
+Name_dossier="./C"
 
-if [ -f "$Name_dossier" ]; then
-   ./$Name_dossier
-else then
-   echo "Le fichier $Name_dossier n'est pas ouvrable"
+# Vérifier si le dossier existe
+if [ -d "$Name_dossier" ]; then
+    # Changer de répertoire vers le dossier du projet
+    (
+        cd "$Name_dossier"
+        if [ -f "Makefile" ]; then
+            echo "Compilation en cours..."
+            make all
+            if [ -f "factorielle" ]; then
+                echo "Compilation réussie, exécutable 'factorielle' créé."
+            else
+                echo "Erreur : L'exécutable 'factorielle' n'a pas été créé."
+            fi
+        else
+            echo "Le Makefile n'est pas présent dans le dossier."
+        fi
+    )
+else
+    echo "Le dossier '$Name_dossier' n'existe pas ou n'est pas accessible."
 fi
 
-while
-done
 
-do
